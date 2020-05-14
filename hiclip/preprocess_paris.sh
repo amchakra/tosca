976456afd38e1c4bf7261c6f83c491a00178fb18
Anob Chakrabarti <anob.chakrabarti@crick.ac.uk>
2020-05-14 23:35:15 +0100

    Updated PARIS preprocessing to include DDNNN as well

--- hiclip/preprocess_paris.sh
+++ hiclip/preprocess_paris.sh
@@ -5,16 +5,19 @@
 # 10th May 2020
 
 # DDDNNXXXXNNNNNN TACCCTTCGCTTCACACACAAG/iSp18/GGATCC/iSp18/TACTGAACCGCNNNNNN
-# Reverse oriented?
 
-# UMI tools will extract the UMI, so need to remove 9 not 15
+# PARIS uses just the hexamer to collapse?
+# However iCLIP uses WWW as well, so here will use DDDNN-NNNNNN as the UMI
+# So will be NNNNNNXXXXNNNNN
+
+# UMI tools will extract the UMI, so need to remove 4 not 15
 
 source activate ehiclip
 
 mkdir rbc
 
 for i in *.fastq.gz; do
-	sbatch -c 8 -t 12:00:00 --wrap="umi_tools extract -p NNNNNNXXXX -I $i -S rbc/${i%%.*}.temp.fastq; \
+	sbatch -c 8 -t 12:00:00 --wrap="umi_tools extract -p NNNNNNXXXXNNNNN -I $i -S rbc/${i%%.*}.temp.fastq; \
 	cutadapt -j 8 -u 9 -m 16 -o rbc/${i%%.*}.rbc.fastq.gz rbc/${i%%.*}.temp.fastq; \
 	rm rbc/${i%%.*}.temp.fastq"
 done
