be4e9f98e1d7ad4f6048b8022a1faac0c46253cc
Nobby Chakrabarti <anob.chakrabarti@crick.ac.uk>
2021-01-15 10:52:24 +0000

    TIdied up old files

--- hiclip/generate_indices.sh
+++
@@ -1,3 +0,0 @@
-sbatch -c 8 -t 12:00:00 --mem=64G --wrap="STAR --runMode genomeGenerate --runThreadN 8 --genomeDir Mm_GencodeM24_rRNA_MT_genes --genomeFastaFiles Mm_GencodeM24_rRNA_MT_genes.fa --limitGenomeGenerateRAM 39550915285"
-
-sbatch -c 8 -t 12:00:00 --wrap="bowtie2-build --threads 8 Mm_GencodeM24_rRNA_MT_genes.fa Mm_GencodeM24_rRNA_MT_genes"
