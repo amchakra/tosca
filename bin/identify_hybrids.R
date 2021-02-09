56a10b1fa016f434ba3fc980a09db7beba15849d
Anob Chakrabarti <anob.chakrabarti@crick.ac.uk>
2021-02-09 20:18:51 +0000

    Added if statement to identify_hybrids.R in case blast output empty

--- bin/identify_hybrids.R
+++ bin/identify_hybrids.R
@@ -12,21 +12,29 @@ option_list <- list(make_option(c("-b", "--blast8"), action = "store", type = "c
 opt_parser = OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
-# Load blast and get read lengths
-blast.dt <- load_blast8(opt$blast8)
-blast.dt <- add_read_lengths(blast.dt = blast.dt, fasta = opt$fasta)
-blast.dt <- calculate_blast8_metrics(blast.dt = blast.dt)
+if(is.na(readLines(opt$blast8)[1])) {
 
-blast.list <- split(blast.dt, blast.dt$query)
-cl <- makeForkCluster(opt$threads) # otherwise really slow...
-hybrids.list <- parLapply(cl = cl, blast.list, function(x) get_valid_hybrids(blast.query.dt = x))
-stopCluster(cl)
+    fwrite(data.table(), file = opt$output, sep = "\t", col.names = TRUE) # Accounts for empty filtered blast file
 
-# message(sum(S4Vectors::elementNROWS(hybrids.list) == 0), " out of ", length(hybrids.list), " reads did not have hybrids")
-# message(round(sum(S4Vectors::elementNROWS(hybrids.list) != 0)/length(hybrids.list), 4) * 100, "% of reads had hybrids")
+} else {
 
-# Filter multi hits
-hybrids.dt <- rbindlist(hybrids.list)
-valid.hybrids.dt <- filter_valid_hybrids(hybrids.dt)
+    # Load blast and get read lengths
+    blast.dt <- load_blast8(opt$blast8)
+    blast.dt <- add_read_lengths(blast.dt = blast.dt, fasta = opt$fasta)
+    blast.dt <- calculate_blast8_metrics(blast.dt = blast.dt)
 
-fwrite(valid.hybrids.dt, file = opt$output, sep = "\t", col.names = TRUE)
+    blast.list <- split(blast.dt, blast.dt$query)
+    cl <- makeForkCluster(opt$threads) # otherwise really slow...
+    hybrids.list <- parLapply(cl = cl, blast.list, function(x) get_valid_hybrids(blast.query.dt = x))
+    stopCluster(cl)
+
+    # message(sum(S4Vectors::elementNROWS(hybrids.list) == 0), " out of ", length(hybrids.list), " reads did not have hybrids")
+    # message(round(sum(S4Vectors::elementNROWS(hybrids.list) != 0)/length(hybrids.list), 4) * 100, "% of reads had hybrids")
+
+    # Filter multi hits
+    hybrids.dt <- rbindlist(hybrids.list)
+    valid.hybrids.dt <- filter_valid_hybrids(hybrids.dt)
+
+    fwrite(valid.hybrids.dt, file = opt$output, sep = "\t", col.names = TRUE)
+
+}
